a4ba838c2ba44e96816ad34554a443d2fbf42b26
Sultan Mahmud <kaziiriad@gmail.com>
2025-05-22 21:02:18 +0600

    created infra template.

---
+++ infra/__main__.py
@@ -0,0 +1,23 @@
+
+
+# VPC
+
+# Subnets - 1 public, 1 private
+
+# Route Tables - 1 public, 1 private
+
+# NAT Gateway
+
+# IGW
+
+# Security Groups - 
+
+# EC2s
+
+# Worker
+
+# Monitor
+
+# API
+
+# Redis
